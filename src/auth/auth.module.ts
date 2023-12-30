80925e22d5ea2e90c46b67c3bb96fb3123c14f19
Marcos Souza <marcos.souza@beeleads.com.br>
2023-12-29 22:31:40 -0300

    feat: Protegendo rotas com Guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { Env } from 'src/env'
+import { JwtEstrategy } from './jwt.strategy'
 
 @Module({
   imports: [
@@ -22,5 +23,6 @@ import { Env } from 'src/env'
       },
     }),
   ],
+  providers: [JwtEstrategy],
 })
 export class AuthModule {}

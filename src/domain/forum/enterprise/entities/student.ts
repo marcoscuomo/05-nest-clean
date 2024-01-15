2a9a52f871d35f2067b8cb658fd6d7f475facbc9
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-14 23:34:55 -0300

    feat: Gateways de criptografia

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,15 +3,22 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
-  // public name: string
+  get name() {
+    return this.props.name
+  }
 
-  // constructor(props: StudentProps, id?: string) {
-  //   super(props, id)
-  //   // this.name = props.name
-  // }
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
 
   static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)

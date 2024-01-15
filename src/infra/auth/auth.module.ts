5ed81e5d32723ffc8f5b26c072ee7aae4a18ec66
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-15 12:35:40 -0300

    feat: Rotas privadas por padrão

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { Env } from '@/infra/env'
 import { JwtEstrategy } from './jwt.strategy'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './jwt-auth.guard'
 
 @Module({
   imports: [
@@ -23,6 +25,12 @@ import { JwtEstrategy } from './jwt.strategy'
       },
     }),
   ],
-  providers: [JwtEstrategy],
+  providers: [
+    JwtEstrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}

eb2564d951089616bf5a6e24d91b2687e677fca3
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-15 10:35:34 -0300

    feat: Testes do cadastro e autenticação

---
+++ test/factories/make-student.ts
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import {
+  Student,
+  StudentProps,
+} from '@/domain/forum/enterprise/entities/student'
+
+export function makeStudent(
+  override: Partial<StudentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const student = Student.create(
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      ...override,
+    },
+    id,
+  )
+
+  return student
+}

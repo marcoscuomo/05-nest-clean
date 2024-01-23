2bc6928fb373823bd376ecb12560ec63ff47215e
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-22 22:19:32 -0300

    feat: Caso de uso upload de anexo

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from '@/core/either'
+import { Injectable } from '@nestjs/common'
+import { InvalidAttachmentType } from './errors/invalid-attachment-type'
+import { Attachment } from '../../enterprise/entities/attachment'
+import { AttachmentsRepository } from '../repositories/attachment-repository'
+import { Uploader } from '../storage/uploader'
+
+interface UploadAndCreateAttachmentUseCaseRequest {
+  fileName: string
+  fileType: string
+  body: Buffer
+}
+
+type UploadAndCreateAttachmentUseCaseResponse = Either<
+  InvalidAttachmentType,
+  {
+    attachment: Attachment
+  }
+>
+@Injectable()
+export class UploadAndCreateAttachmentUseCase {
+  constructor(
+    private attachmentsRepository: AttachmentsRepository,
+    private uploader: Uploader,
+  ) {}
+
+  async execute({
+    fileName,
+    fileType,
+    body,
+  }: UploadAndCreateAttachmentUseCaseRequest): Promise<UploadAndCreateAttachmentUseCaseResponse> {
+    if (!/^image\/(jpeg|pjpeg)|application\/pdf|image\/png$/.test(fileType)) {
+      return left(new InvalidAttachmentType(fileType))
+    }
+
+    // Envio para um storage s3 exemplo
+    const { url } = await this.uploader.upload({
+      fileName,
+      fileType,
+      body,
+    })
+
+    const attachment = Attachment.create({
+      title: fileName,
+      url,
+    })
+
+    await this.attachmentsRepository.create(attachment)
+
+    return right({
+      attachment,
+    })
+  }
+}

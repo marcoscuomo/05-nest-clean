e2570ba4c1de81fe014ded9bd09e557c5586e01c
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-24 20:50:27 -0300

    feat: Respostas com anexo

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -14,6 +14,10 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async create(answer: Answer) {
     this.items.push(answer)
 
+    await this.answerAttachmentsRepository.createMany(
+      answer.attachments.getItems(),
+    )
+
     DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
@@ -37,9 +41,17 @@ export class InMemoryAnswersRepository implements AnswersRepository {
   async save(answer: Answer): Promise<void> {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
-    DomainEvents.dispatchEventsForAggregate(answer.id)
-
     this.items[itemIndex] = answer
+
+    await this.answerAttachmentsRepository.createMany(
+      answer.attachments.getNewItems(),
+    )
+
+    await this.answerAttachmentsRepository.deleteMany(
+      answer.attachments.getRemovedItems(),
+    )
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async findManyByQuestionId(
